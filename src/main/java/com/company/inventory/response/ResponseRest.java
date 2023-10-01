dd3e7465e23317434a671de918a1212d1953b554
Jorge Meza Bello <jorberiver@gmail.com>
2023-09-30 22:49:57 -0600

    añadida metada

---
+++ src/main/java/com/company/inventory/response/ResponseRest.java
@@ -0,0 +1,19 @@
+package com.company.inventory.response;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ResponseRest {
+	
+	private ArrayList<HashMap<String, String>> metadata = new ArrayList<>();
+	
+	public void setMetadata(String type, String code, String date) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("type", type);
+		map.put("code", code);
+		map.put("date", date);
+		metadata.add(map);
+	}
+	
+	
+}
