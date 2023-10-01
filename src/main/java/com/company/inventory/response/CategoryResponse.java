af0ba06be094447f4ec74b196da0f657ed22ed15
Jorge Meza Bello <jorberiver@gmail.com>
2023-09-30 23:23:33 -0600

    estructura de respuesta lista

---
+++ src/main/java/com/company/inventory/response/CategoryResponse.java
@@ -0,0 +1,38 @@
+package com.company.inventory.response;
+
+import java.util.List;
+import java.util.Objects;
+
+import com.company.inventory.model.Category;
+
+public class CategoryResponse {
+	
+	private List<Category> category;
+	
+	public CategoryResponse(List<Category> category) {
+		super();
+		this.category = category;
+	}
+
+	public List<Category> getCategory() {
+		return category;
+	}
+
+	public void setCategory(List<Category> category) {
+		this.category = category;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoryResponse other = (CategoryResponse) obj;
+		return Objects.equals(category, other.category);
+	}
+	
+
+}
