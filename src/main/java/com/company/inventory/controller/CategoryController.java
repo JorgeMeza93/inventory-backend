116c20cfbae8cdbe9e4a31f19537261cac449f5f
Jorge Meza Bello <jorberiver@gmail.com>
2023-11-20 02:12:33 -0600

    Añadida configuración de CORS

--- src/main/java/com/company/inventory/controller/CategoryController.java
+++ src/main/java/com/company/inventory/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.company.inventory.service.ICategoryService;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CategoryController {
 	
 	@Autowired
